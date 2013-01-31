889949b91621594d0d8ea9120e5de687a2269f63
Joe Donley <joe@jdonley83.com>
2013-01-30 20:07:34 -0600

    at symbol now removed from the beginning of strings

--- g-filter-commander.js
+++ g-filter-commander.js
@@ -2,8 +2,20 @@ com = {};
 com.jdonley83 = {};
 com.jdonley83.g_filter_commander = (function(){
 	function sanitize_data(input) {
-		var filter_arr = input.split("|");
-		return filter_arr;
+		var filter_arr = input.split("|"),
+			output_arr = [],
+			length = filter_arr.length;
+
+		for (var i = 0; i < length; i++) {
+			var current_item = filter_arr[i];
+			if (current_item.indexOf("@") == 0) {
+				output_arr[i] = current_item.substring(1);
+			} else {
+				output_arr[i] = current_item;
+			}
+		}
+
+		return output_arr;
 	}
 
 	return {
